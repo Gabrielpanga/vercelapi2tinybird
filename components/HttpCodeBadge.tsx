7fcd6e397d1f397ac2f6d2640e9a45fb7b78ef79
Gabriel Pan Gantes <gabrielpanga@gmail.com>
2022-10-23 13:06:42 -0300

    fix: tailwind styles

--- components/HttpCodeBadge.tsx
+++ components/HttpCodeBadge.tsx
@@ -1,23 +1,22 @@
 import { STATUS_CODES } from 'http'
 
 export default function HttpCodeBadge({ code }: { code: number }) {
-  const baseStyles = 'text-sm font-medium mr-2 px-2 py-1 rounded'
+  const baseStyles = 'text-sm font-medium mr-2 px-2 py-1 rounded text-slate-100'
   const statusCodeText = STATUS_CODES[code]
   let color = 'zinc'
+
   if (code >= 200 && code < 300) {
-    color = 'green'
+    color = 'bg-green-500 dark:bg-green-200 dark:text-green-900'
   } else if (code >= 300 && code < 400) {
-    color = 'indigo'
+    color = 'bg-indigo-500 dark:bg-indigo-200 dark:text-indigo-900'
   } else if (code >= 400 && code < 500) {
-    color = 'rose'
+    color = 'bg-rose-500 dark:bg-rose-200 dark:text-rose-900'
   } else {
-    color = 'orange'
+    color = 'bg-orange-500 dark:bg-orange-200 dark:text-orange-900'
   }
 
   return (
-    <span
-      className={`bg-${color}-500 text-slate-100 dark:bg-${color}-200 dark:text-${color}-900 ${baseStyles}`}
-    >
+    <span className={`${color} ${baseStyles}`}>
       {code} - {statusCodeText}
     </span>
   )
